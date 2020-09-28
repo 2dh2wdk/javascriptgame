1cb2bd85560a248696513320d3fbae88338fca58
2dh2wdk <2dh2wdk2@naver.com>
2020-09-28 17:27:06 +0900

    092801

---
+++ wordrelay/index.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector('#button')
+// document.querySelector(아이디)는 그 아이디의 태그를 가져오는 것
+btn.addEventListener('click', () => {
+    const word = document.querySelector('#word').textContent
+    // .은 ~의 라는 의미하는 것과 같다.
+    const input = document.querySelector('#input').value
+    const lastIndex= word.length - 1
+    if(input[0]=word[lastIndex]){
+
+    }
+    input[0]
+    word[word.length-1]
+    //string(문자열),number(숫자),boolean(T/F),object(객체),null,undefined,symbol
+})
