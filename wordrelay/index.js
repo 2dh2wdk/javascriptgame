36b018a64a11a1350ccd793d0abb7190ea1524e9
2dh2wdk <2dh2wdk2@naver.com>
2020-09-29 00:47:43 +0900

    2020092901

--- wordrelay/index.js
+++ wordrelay/index.js
@@ -1,14 +1,24 @@
-const btn = document.querySelector('#button')
-// document.querySelector(아이디)는 그 아이디의 태그를 가져오는 것
-btn.addEventListener('click', () => {
-    const word = document.querySelector('#word').textContent
-    // .은 ~의 라는 의미하는 것과 같다.
-    const input = document.querySelector('#input').value
-    const lastIndex= word.length - 1
-    if(input[0]=word[lastIndex]){
+const btn = document.querySelector('#button') // document.querySelector(아이디)는 그 아이디의 태그를 가져오는 것
+const inputTag = document.querySelector('#input')
+inputTag.focus()
 
+btn.addEventListener('click', () => {
+    const wordTag = document.querySelector('#word')
+    const word = wordTag.textContent    // .은 ~의 라는 의미하는 것과 같다.
+    const inputTag = document.querySelector('#input')
+    const input = inputTag.value
+    const errorTag = document.querySelector('#error')
+    const lastIndex= word.length - 1
+    const w = word[lastIndex]
+    const i = input[0]
+    if(w === i){
+        wordTag.textContent = input
+        errorTag.textContent = ''
+        inputTag.value = ''
+        inputTag.focus()
+    } else{
+        errorTag.textContent = '땡'
+        inputTag.focus()
     }
-    input[0]
-    word[word.length-1]
     //string(문자열),number(숫자),boolean(T/F),object(객체),null,undefined,symbol
 })
