698863ce0fac4804d2d6743329b1cc0562cd932c
2dh2wdk <2dh2wdk2@naver.com>
2020-09-28 15:21:39 +0900

    0928

--- gugudan/index.js
+++ gugudan/index.js
@@ -1,15 +1,15 @@
-document.querySelector('#click').addEventListener('click', ()=>{
+document.querySelector('#click').addEventListener('click', () => {
+    const a = document.querySelector('#first').value
+    const b = document.querySelector('#second').value
+    const r = document.querySelector('#result')
 
-const a = document.querySelector('#first').value
-const b = document.querySelector('#second').value
-
-if(a){
-    if(b){
-        document.querySelector('#result').textContent = a*b
-    } else{
-        document.querySelector('#result').textContent = '두번째 값을 입력하시오'
+    if (a) {
+        if (b) {
+            r.textContent = a * b
+        } else {
+            r.textContent = '두번째 값을 입력하시오'
+        }
+    } else {
+        r.textContent = '첫번째 값을 입력하시오'
     }
-} else {
-    document.querySelector('#result').textContent = '첫번째 값을 입력하시오'
-}
 })
